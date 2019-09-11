8ce6c153da9d5724367dfb0c7a1b02db4ced4cad
tnk1422 <tarak.konar@gmail.com>
2019-09-10 22:49:54 -0500

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://381b69b3.us-south.apiconnect.appdomain.cloud/team';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
